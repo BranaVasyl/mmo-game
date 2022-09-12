805aa2d864c2a7b252a1bf2d53023edc32fe33d8
BranaVasyl <93434029+BranaVasyl@users.noreply.github.com>
2022-09-12 22:29:02 +0200

    start creating spells

---
+++ MMOClient/Assets/_Code/Utility/Notes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BV
+{
+    public class Notes : MonoBehaviour
+    {
+        [TextArea(15, 100)]
+        public string notes;
+    }
+}
